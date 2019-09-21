f45e717c5af6294f6fa5b299952dd1871c4cf8d2
crisi <crisi@LAPTOP-IOQ366T5>
2019-09-21 17:09:15 +0200

    TEW- 2.3 Servlets con contexto

--- src/com/tew/Servlets/HolaMundoServlet.java
+++ src/com/tew/Servlets/HolaMundoServlet.java
@@ -31,6 +31,8 @@ public class HolaMundoServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		String nombre = (String) request.getParameter("NombreUsuario");
 		PrintWriter out = response.getWriter();
+		response.setCharacterEncoding("UTF-8");
+		response.setContentType("text/html");
 		@SuppressWarnings("unchecked")
 		Vector<String> listado = (Vector<String>)request.getSession().getAttribute("listado");
 		if (listado == null){listado = new Vector<String>();}
@@ -41,15 +43,14 @@ public class HolaMundoServlet extends HttpServlet {
 		out.println("<br>");
 		out.println("Contigo, hoy me han visitado:<br>");
 		for ( int i = 0 ; i < listado.size() ; i++ ){out.println("<br>"+(String)listado.elementAt(i));}
-		out.println("<a href=\"index.html\">volver</a>");
-		response.setCharacterEncoding("UTF-8");
-		response.setContentType("text/html");
-		out.println("<HTML>");
-		out.println("<HEAD><TITLE>Hola Mundo!</TITLE></HEAD>");
-		out.println("<BODY>");
-		if ( nombre != null ){out.println("<br>Hola "+nombre+"<br>");}
-		out.println("Bienvenido a mi primera página web!");
-		out.println("</BODY></HTML>");
+		Integer contador= (Integer) getServletContext().getAttribute("contador");
+		if ( contador == null ){contador = new Integer(0);}
+		// Establecemos el contador como atributo del contextbajo el nombre 
+		//contador. En caso de que ya existiera, sobreescribiríala referencia
+		// existentecon la nueva.
+		getServletContext().setAttribute("contador", new Integer(contador.intValue()+1));
+		out.println("<br><br>" + contador +" visitas");
+		out.println("<br><a href=\"index.html\">volver</a>");
 	}
 
 	/**
